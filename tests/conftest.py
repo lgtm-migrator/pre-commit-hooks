ff9a9be78ee3f56ee36e4252d467a32e8278a9a0
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2021-01-05 10:40:51 +0000

    Use betamax to record interactions with PyPI API

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,1 +1,25 @@
+# 3rd party
+import pytest
+from _pytest.fixtures import FixtureRequest
+from shippinglabel.pypi import PYPI_API
+
 pytest_plugins = ("domdf_python_tools.testing", )
+# 3rd party
+from betamax import Betamax  # type: ignore
+from domdf_python_tools.paths import PathPlus
+
+with Betamax.configure() as config:
+	config.cassette_library_dir = PathPlus(__file__).parent / "cassettes"
+
+
+@pytest.fixture()
+def cassette(request: FixtureRequest):
+	"""
+	Provides a Betamax cassette scoped to the test function
+	which record and plays back interactions with the PyPI API.
+	"""  # noqa: D400
+
+	with Betamax(PYPI_API._store["session"]) as vcr:
+		vcr.use_cassette(request.node.name, record="none")
+
+		yield PYPI_API
--- tests/test_bind_requirements.py
+++ tests/test_bind_requirements.py
@@ -7,36 +7,43 @@ from pre_commit_hooks.util import FAIL, PASS
 
 
 @pytest.mark.parametrize(
-		("input_s", "expected_retval", "output"),
-		(
-				('', PASS, ''),
-				('\n', PASS, '\n'),
-				('# intentionally empty\n', PASS, '# intentionally empty\n'),
-				('foo\n# comment at end\n', FAIL, '# comment at end\nfoo>=.1\n'),
-				('foo\nbar\n', FAIL, 'bar>=0.2.1\nfoo>=.1\n'),
-				('bar\nfoo\n', FAIL, 'bar>=0.2.1\nfoo>=.1\n'),
-				('a\nc\nb\n', FAIL, 'a>=1.0\nb>=1.0.0\nc>=0.1.0\n'),
-				('a\nb\nc', FAIL, 'a>=1.0\nb>=1.0.0\nc>=0.1.0\n'),
-				(
+		"input_s, expected_retval, output",
+		[
+				pytest.param('', PASS, '', id="empty"),
+				pytest.param('\n', PASS, '\n', id="newline_only"),
+				pytest.param('# intentionally empty\n', PASS, '# intentionally empty\n', id="intentionally_empty"),
+				pytest.param('foo\n# comment at end\n', FAIL, '# comment at end\nfoo>=.1\n', id="comment_at_end"),
+				pytest.param('foo\nbar\n', FAIL, 'bar>=0.2.1\nfoo>=.1\n', id="foo_bar"),
+				pytest.param('bar\nfoo\n', FAIL, 'bar>=0.2.1\nfoo>=.1\n', id="bar_foo"),
+				pytest.param('a\nc\nb\n', FAIL, 'a>=1.0\nb>=1.0.0\nc>=0.1.0\n', id="a_c_b"),
+				pytest.param('a\nb\nc', FAIL, 'a>=1.0\nb>=1.0.0\nc>=0.1.0\n', id="a_b_b"),
+				pytest.param(
 						'#comment1\nfoo\n#comment2\nbar\n',
 						FAIL,
 						'#comment1\n#comment2\nbar>=0.2.1\nfoo>=.1\n',
+						id="comment_foo_comment_bar"
 						),
-				(
+				pytest.param(
 						'#comment1\nbar\n#comment2\nfoo\n',
 						FAIL,
 						'#comment1\n#comment2\nbar>=0.2.1\nfoo>=.1\n',
+						id="comment_bar_comment_foo"
 						),
-				('#comment\n\nfoo\nbar\n', FAIL, '#comment\nbar>=0.2.1\nfoo>=.1\n'),
-				('#comment\n\nbar\nfoo\n', FAIL, '#comment\nbar>=0.2.1\nfoo>=.1\n'),
-				('\nfoo\nbar\n', FAIL, 'bar>=0.2.1\nfoo>=.1\n'),
-				('\nbar\nfoo\n', FAIL, 'bar>=0.2.1\nfoo>=.1\n'),
-				(
+				pytest.param(
+						'#comment\n\nfoo\nbar\n', FAIL, '#comment\nbar>=0.2.1\nfoo>=.1\n', id="comment_foo_bar"
+						),
+				pytest.param(
+						'#comment\n\nbar\nfoo\n', FAIL, '#comment\nbar>=0.2.1\nfoo>=.1\n', id="comment_barfoo_"
+						),
+				pytest.param('\nfoo\nbar\n', FAIL, 'bar>=0.2.1\nfoo>=.1\n', id="foo_bar_2"),
+				pytest.param('\nbar\nfoo\n', FAIL, 'bar>=0.2.1\nfoo>=.1\n', id="bar_foo_2"),
+				pytest.param(
 						'pyramid-foo==1\npyramid>=2\n',
 						FAIL,
 						'pyramid>=2\npyramid-foo==1\n',
+						id="pyramid-foo_pyramid"
 						),
-				(
+				pytest.param(
 						'a==1\n'
 						'c>=1\n'
 						'bbbb!=1\n'
@@ -54,21 +61,38 @@ from pre_commit_hooks.util import FAIL, PASS
 						'e>=2\n'
 						'f<=2\n'
 						'g<2\n',
+						id="a-g",
 						),
-				('ocflib\nDjango\nPyMySQL\n', FAIL, 'django>=3.1.3\nocflib>=2020.10.9.8.6\npymysql>=0.10.1\n'),
-				('bar\npkg-resources==0.0.0\nfoo\n', FAIL, 'bar>=0.2.1\nfoo>=.1\npkg-resources==0.0.0\n'),
-				('foo\npkg-resources==0.0.0\nbar\n', FAIL, 'bar>=0.2.1\nfoo>=.1\npkg-resources==0.0.0\n'),
-				('foo???1.2.3\nbar\n', FAIL, 'foo???1.2.3\nbar>=0.2.1\n'),
-				(
+				pytest.param(
+						'ocflib\nDjango\nPyMySQL\n',
+						FAIL,
+						'django>=3.1.5\nocflib>=2020.12.5.10.49\npymysql>=0.10.1\n',
+						id="real_requirements"
+						),
+				pytest.param(
+						'bar\npkg-resources==0.0.0\nfoo\n',
+						FAIL,
+						'bar>=0.2.1\nfoo>=.1\npkg-resources==0.0.0\n',
+						id="bar_pkg-resources_foo"
+						),
+				pytest.param(
+						'foo\npkg-resources==0.0.0\nbar\n',
+						FAIL,
+						'bar>=0.2.1\nfoo>=.1\npkg-resources==0.0.0\n',
+						id="foo_pkg-resources_bar"
+						),
+				pytest.param('foo???1.2.3\nbar\n', FAIL, 'foo???1.2.3\nbar>=0.2.1\n', id="bad_specifiers"),
+				pytest.param(
 						'wxpython>=4.0.7; platform_system == "Windows" and python_version < "3.9"\n'
 						'wxpython>=4.0.7; platform_system == "Darwin" and python_version < "3.9"\n',
 						PASS,
 						'wxpython>=4.0.7; platform_system == "Windows" and python_version < "3.9"\n'
 						'wxpython>=4.0.7; platform_system == "Darwin" and python_version < "3.9"\n',
+						id="markers",
 						),
-				),
+				]
 		)
-def test_integration(input_s, expected_retval, output, tmpdir):
+def test_integration(input_s, expected_retval, output, tmpdir, cassette):
 	path = tmpdir.join("file.txt")
 	path.write_text(input_s, encoding="UTF-8")
 
